be3a2fd231d7886e6f64a5f77fa591e091ce3557
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-03-16 12:37:57 -0400

    Add work script to vc to keep track of progress

---
+++ work-aju.py
@@ -0,0 +1,93 @@
+import ajustador as aju, measurements1 as ms1, numpy as np
+from ajustador import drawing
+from matplotlib import pyplot
+
+# waves1h = ms1.waves1[ms1.waves1.injection <= 0][::3]
+# waves2h = ms1.waves2[ms1.waves2.injection <= 0][::3]
+# waves3h = ms1.waves3[ms1.waves3.injection <= 0][::3]
+# waves4h = ms1.waves4[ms1.waves4.injection <= 0][::3]
+# waves5h = ms1.waves5[ms1.waves5.injection <= 0][::3]
+# waves6h = ms1.waves6[ms1.waves6.injection <= 0][::3]
+# waves7h = ms1.waves7[ms1.waves7.injection <= 0][::3]
+# waves8h = ms1.waves8[ms1.waves8.injection <= 0][::3]
+waves9h = ms1.waves9[ms1.waves9.injection <= 0][::3]
+
+params = aju.optimize.ParamSet(
+    ('RA',                 4.309,    0, 100),
+    ('RM',                 0.722,    0,  10),
+    ('CM',                 0.015,    0, 0.10),
+    ('Cond_D1_Kir',        9.4644,   0, 100),
+    ('Kir_offset',         0,        -0.005, +0.005),
+    ('morph_file', 'MScell-tertDendlongRE.p'),
+    ('neuron_type', 'D1'))
+
+fit = aju.optimize.Fit('../fit-2017-aju-cma-wave9h-1',
+                       waves9h, aju.fitnesses.hyperpol_fitness, params)
+fit.load()
+fit.do_fit(100)
+
+
+params2 = params.update(RA=3.309,
+                        RM=1.722,
+                        CM=0.025,
+                        Cond_D1_Kir=1.4644,
+                        Kir_offset=0)
+fit2 = aju.optimize.Fit('../fit-2017-aju-cma-wave9h-2',
+                       waves9h, aju.fitnesses.hyperpol_fitness, params2)
+fit2.load()
+fit2.do_fit(100)
+
+
+params3 = aju.optimize.ParamSet(
+    ('junction_potential', 0,       -0.030, +0.030),
+    ('RA',                 12.004,    0, 100),
+    ('RM',                 9.427,     0,  10),
+    ('CM',                 0.03604,   0, 0.10),
+    ('Cond_D1_Kir',        14.502,    0, 100),
+    ('Kir_offset',         -.004,    -0.005, +0.005),
+    ('morph_file', 'MScell-tertDendlongRE.p'),
+    ('neuron_type', 'D1'))
+fit3 = aju.optimize.Fit('../fit-2017-aju-cma-wave9h-3',
+                        waves9h, aju.fitnesses.hyperpol_fitness, params3)
+fit3.load()
+fit3.do_fit(100)
+
+
+params4 = params3
+fit4 = aju.optimize.Fit('../fit-2017-aju-cma-wave5h-1',
+                        ms1.waves5[0:10:2], aju.fitnesses.hyperpol_fitness, params4)
+fit4.load()
+fit4.do_fit(100)
+
+
+params5 = params3
+aju.fitnesses.ERROR = aju.fitnesses.ErrorCalc.relative
+fit5 = aju.optimize.Fit('../fit-2017-aju-cma-wave5h-2',
+                        ms1.waves5[0:10:2], aju.fitnesses.hyperpol_fitness, params5)
+fit5.load()
+fit5.do_fit(100)
+
+for i in range(len(fit5)):
+    print(i, fit5.fitness_func(fit5[i], fit5.measurement, full=1),
+          '→', fit5.fitness_func(fit5[i], fit5.measurement, full=0))
+
+
+aju.fitnesses.ERROR = aju.fitnesses.ErrorCalc.relative
+params6 = aju.optimize.ParamSet(
+    ('junction_potential', 0,       -0.030, +0.030),
+    ('RA',                 12.004,    0, 100),
+    ('RM',                 9.427,     0,  10),
+    ('CM',                 0.03604,   0, 0.10),
+    ('Cond_D1_Kir',        14.502,    0, 100),
+    ('Kir_offset',         -.004,    -0.005, +0.005),
+    ('morph_file', 'MScell-tertDendlongRE.p'),
+    ('neuron_type', 'D1'),
+    ('Cond_D1_NaF_0',      150e3,      0, 600e3),
+    ('Cond_D1_KaS_0',      372,        0, 600),
+    ('Cond_D1_KaF_0',      641,        0, 1000),
+    ('Cond_D1_Krp_0',      177,        0, 600))
+fit6 = aju.optimize.Fit('../fit-2017-aju-cma-wave5-2',
+                        ms1.waves5[[0, 3, 7, 13, 17, 19, 21, 22, 23]],
+                        aju.fitnesses.new_combined_fitness, params6)
+fit6.load()
+fit6.do_fit(150, popsize=20)
