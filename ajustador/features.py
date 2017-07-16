features: fix calculation of baseline w/o noise
For a measurement without any noise (050511-3ivifcurves [1]) baseline
was calculated as nan±nan, because 40th and 60th percentile were the
same. Chaning the equality type fixes the issue.

ajustador/features.py
@@ -133,7 +133,7 @@ class SteadyState(Feature):
                   (wave.x > after if after is not None else False))
         what = wave.y[region]
         cutoffa, cutoffb = np.percentile(what, (40, 60))
-        cut = what[(what > cutoffa) & (what < cutoffb)]
+        cut = what[(what >= cutoffa) & (what <= cutoffb)]
         return vartype.array_mean(cut)
 
     @property