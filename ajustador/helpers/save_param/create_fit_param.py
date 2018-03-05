f9966073b30cbd155baa01b1bb7df0270a1cf30f
sriram161 <skappag@masonlive.gmu.edu>
2018-03-05 07:48:17 -0500

    Renamed save_param_npz.py to createt_npz_param.py and removed load_npz function.

---
+++ ajustador/helpers/save_param/create_fit_param.py
@@ -0,0 +1,10 @@
+import logging
+import fileinput
+import shutil
+import numpy as np
+import re
+import sys
+from pathlib import Path
+from datetime import datetime
+from ajustador.helpers.loggingsystem import getlogger
+
