daea3075fc04e3bbe7d747249fa638aa9138d7a6
sriram161 <skappag@masonlive.gmu.edu>
2018-04-03 14:22:57 -0400

    Code refactor and execution steps are grouped into functions.

--- ajustador/helpers/save_param/create_npz_param.py
+++ ajustador/helpers/save_param/create_npz_param.py
@@ -7,18 +7,10 @@
 """
 
 import logging
-import fileinput
-import shutil
-import sys
-import re
 import numpy as np
 from pathlib import Path
 from ajustador.helpers.loggingsystem import getlogger
-from ajustador.helpers.save_param.process_morph import find_morph_file
-from ajustador.helpers.save_param.process_morph import get_morph_file_name
-from ajustador.helpers.save_param.process_morph import update_morph_file_name
 from ajustador.helpers.save_param.process_param_cond import get_state_machine
-from ajustador.helpers.save_param.process_param_cond import process_cond_line
 from ajustador.helpers.save_param.support import create_path
 from ajustador.helpers.save_param.support import get_least_fitness_params
 from ajustador.helpers.save_param.support import get_conds_non_conds
@@ -26,74 +18,18 @@ from ajustador.helpers.save_param.support import get_file_abs_path
 from ajustador.helpers.save_param.support import make_cond_file_name
 from ajustador.helpers.save_param.support import get_file_name_with_version
 from ajustador.helpers.save_param.support import check_version_build_file_path
+from ajustador.helpers.save_param.support import extract_morph_file_from_cond
+from ajustador.helpers.save_param.support import make_model_path_obj
+from ajustador.helpers.save_param.support import exercise_machine_on_cond
+from ajustador.helpers.save_param.support import update_morph_file_name_in_cond
+from ajustador.helpers.save_param.support import clone_param_cond_file
+from ajustador.helpers.save_param.support import clone_and_change_morph_file
 
 logger = getlogger(__name__)
 logger.setLevel(logging.INFO)
 
-def extract_morph_file_from_cond(cond_file_path, neuron_type):
-    with fileinput.input(files=(cond_file_path)) as f_obj:
-       for line in f_obj:
-           if find_morph_file(line):
-               return get_morph_file_name(line, neuron_type)
-
-def make_model_path_obj(model_path, model):
-    Object = lambda **kwargs: type("Object", (), kwargs)
-    return Object(__file__ = str(model_path), value = model)
-
-def exercise_machine_on_cond(machine, cond_file_path, header_line):
-    with fileinput.input(files=(cond_file_path), inplace=True) as f_obj:
-       machine = machine
-       header_not_written = True
-       for line in f_obj:
-           if header_not_written:
-               sys.stdout.write(header_line)
-               header_not_written = False
-           process_cond_line(line, machine)
-
-def update_morph_file_name_in_cond(cond_file, neuron_type, morph_file_name):
-    logger.debug("\n {}".format(cond_file))
-    with fileinput.input(files=(cond_file), inplace=True) as f_obj:
-         for line in f_obj:
-           if find_morph_file(line):
-              line = update_morph_file_name(line, neuron_type, morph_file_name)
-           sys.stdout.write(line)
-
-def clone_param_cond_file(src_path, src_file, dest_file):
-    ''' Inputs param_cond_file == string => Absolute path of cond_file.
-    model_path == Path objects => model Path objects.
-    model == string => model name.
-    neuron_type == string => Neuron model.
-    '''
-    src_abs_path = get_file_abs_path(src_path, src_file)
-    if Path(dest_file).is_file(): # Creates a version file of destination file.
-        dest_file = get_file_name_with_version(dest_file)
-        shutil.copy(src_abs_path, dest_file)
-        return dest_file
-    shutil.copy(src_abs_path, dest_file)
-    return dest_file
-
-def clone_and_change_morph_file(param_cond_file, model_path, model, neuron_type, non_conds):
-    ''' Inputs param_cond_file == string => Absolute path of cond_file.
-               model_path == Path objects => model Path objects.
-               model == string => model name.
-               neuron_type == string => Neuron model.
-    '''
-    from ajustador.basic_simulation import morph_morph_file
-    morph_features = ('RM', 'Eleak', 'RA', 'CM')
-    model_obj = make_model_path_obj(model_path, model)
-    morph_file = extract_morph_file_from_cond(param_cond_file, neuron_type)
-    src_morph_file_path = get_file_abs_path(model_path, morph_file)
-    if 'conductance_save' in src_morph_file_path:
-        new_morph_file_path = get_file_name_with_version(src_morph_file_path)
-        morph_morph_file(model_obj, neuron_type, src_morph_file_path, new_file = open(new_morph_file_path,'w'),
-        **{k:v for k,v in non_conds.items() if k in morph_features})
-        return new_morph_file_path
-    morph_morph_file(model_obj, neuron_type, src_morph_file_path, new_file = open(str(model_path/'conductance_save'/morph_file),'w'),
-    **{k:v for k,v in non_conds.items() if k in morph_features})
-    return str(model_path/'conductance_save'/morph_file)
-
 def create_npz_param(npz_file, model, neuron_type, store_param_path=None,
-                     fitnum=None, cond_file='param_cond.py'):
+                     fitnum=None, cond_file= None):
     """Main function to be executed to generate parameter file from npz_file.
        Inputs => *.npz file; model can be 'gp', 'd1d2', 'ep' or 'ca1' soon;
                  neuron_type can be 'proto', 'D1' or 'D2' soon;
@@ -116,13 +52,14 @@ def create_npz_param(npz_file, model, neuron_type, store_param_path=None,
     conds, non_conds = get_conds_non_conds(param_data_list)
 
     new_param_path = create_path(store_param_path) if store_param_path else create_path(model_path/'conductance_save')
-    if cond_file != 'param_cond.py':
-        new_param_cond = new_param_path / cond_file
-    else:
-        new_param_cond = make_cond_file_name(data, npz_file, str(new_param_path), neuron_type)
+    if cond_file is None:
+        cond_file = 'param_cond.py'
+    new_param_cond = make_cond_file_name(data, npz_file,
+                         str(new_param_path), neuron_type, cond_file)
 
-    logger.info("START STEP 3!!! Copy \n source : {} \n dest: {}".format(get_file_abs_path(model_path,cond_file), new_param_cond))
-    new_param_cond = clone_param_cond_file(src_path=model_path, src_file=cond_file, dest_file=new_param_cond)
+    new_cond_file_name = check_version_build_file_path(str(new_param_cond), neuron_type, fit_number)
+    logger.info("START STEP 3!!! Copy \n source : {} \n dest: {}".format(get_file_abs_path(model_path,cond_file), new_cond_file_name))
+    new_param_cond = clone_param_cond_file(src_path=model_path, src_file=cond_file, dest_file=new_cond_file_name)
 
     logger.info("START STEP 4!!! Extract and modify morph_file from {}".format(new_param_cond))
     morph_file = clone_and_change_morph_file(new_param_cond, model_path, model, neuron_type, non_conds)
@@ -130,13 +67,10 @@ def create_npz_param(npz_file, model, neuron_type, store_param_path=None,
 
     logger.info("START STEP 6!!! Modify the param file {}".format(new_param_cond))
     machine = get_state_machine(model, neuron_type, conds)
-    exercise_machine_on_cond(machine, new_param_cond,
-                             header_line)
+    exercise_machine_on_cond(machine, new_param_cond, header_line)
 
     logger.info("START STEP 7!!! Renaming morph and param_cond files.")
-    new_cond_file_name = check_version_build_file_path(new_param_cond, neuron_type, fit_number)
     new_morph_file_name = check_version_build_file_path(morph_file, neuron_type, fit_number)
-    Path(str(new_param_cond)).rename(str(new_cond_file_name))
     Path(str(new_param_path/morph_file)).rename(str(new_morph_file_name))
     logger.info("END STEP 7!!! New files names \n morph: {1} \n param_cond files: {0}".format(new_cond_file_name, new_morph_file_name))
 
--- ajustador/helpers/save_param/support.py
+++ ajustador/helpers/save_param/support.py
@@ -1,10 +1,17 @@
 import logging
+import fileinput
 import re
 import os
+import shutil
+import sys
 import numpy as np
 from pathlib import Path
 
 from ajustador.helpers.loggingsystem import getlogger
+from ajustador.helpers.save_param.process_morph import find_morph_file
+from ajustador.helpers.save_param.process_morph import get_morph_file_name
+from ajustador.helpers.save_param.process_param_cond import process_cond_line
+from ajustador.helpers.save_param.process_morph import update_morph_file_name
 
 logger = getlogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -41,21 +48,23 @@ def get_file_abs_path(model_path, file_):
 def check_key_in_npz_data(npz_data, key):
     if key in npz_data.files:
          return True
-    # I want to show key is not present in npz file
     logger.error("No KEY {} in optimization npz data load!!!".format(key))
     return False
 
-def make_cond_file_name(npz_data, npz_file_name, dest_path, neuron_type):
+def make_cond_file_name(npz_data, npz_file_name, dest_path, neuron_type, cond_file):
     "Makes new cond file name from npz data"
     logger.debug("{} {} {} {}".format(npz_data, npz_file_name, dest_path, neuron_type))
     if check_key_in_npz_data(npz_data,'neuron_type') \
-    and check_key_in_npz_data(npz_data,'measurment_name'):
+    and check_key_in_npz_data(npz_data,'measurment_name'): # may be removed in future.
        if npz_data['neuron_type'] in npz_data['measurment_name']:
-           file_name = 'param_cond_'+ npz_data['measurment_name'] + '.py'
+           file_name = cond_file.rstrip('.py') + '_' + npz_data['measurment_name'] + '.py'
+           logger.debug("1 {} {}".format(cond_file.strip('.py'), file_name))
            return os.path.join(dest_path, file_name)
-       file_name = 'param_cond_' + npz_data['measurment_name'] + npz_data['neuron_type'] + '.py'
+       file_name = cond_file.rstrip('.py') + '_' + npz_data['measurment_name'] + npz_data['neuron_type'] + '.py'
+       logger.debug("2 {} {}".format(cond_file.strip('.py'), file_name))
        return os.path.join(dest_path, file_name)
-    file_name = npz_file_name.rpartition('-' + neuron_type + '-')[2].rstrip('.npz') + '.py'
+    file_name = cond_file.rstrip('.py') + '_' + npz_file_name.rpartition('-' + neuron_type + '-')[2].rstrip('.npz') + '.py'
+    logger.debug("3 {} {}".format(cond_file.rstrip('.py'), file_name))
     return os.path.join(dest_path, file_name)
 
 def get_file_name_with_version(file_):
@@ -79,3 +88,65 @@ def check_version_build_file_path(file_, neuron_type, fit_number):
         abs_file_name = abs_file_name.strip('_'+ post_fix)
         return '_'.join([abs_file_name, neuron_type, str(fit_number), post_fix]) + _ + extn
     return '_'.join([abs_file_name, neuron_type, str(fit_number)]) + _ + extn
+
+def extract_morph_file_from_cond(cond_file_path, neuron_type):
+    with fileinput.input(files=(cond_file_path)) as f_obj:
+       for line in f_obj:
+           if find_morph_file(line):
+               return get_morph_file_name(line, neuron_type)
+
+def make_model_path_obj(model_path, model):
+    Object = lambda **kwargs: type("Object", (), kwargs)
+    return Object(__file__ = str(model_path), value = model)
+
+def exercise_machine_on_cond(machine, cond_file_path, header_line):
+    with fileinput.input(files=(cond_file_path), inplace=True) as f_obj:
+       machine = machine
+       header_not_written = True
+       for line in f_obj:
+           if header_not_written:
+               sys.stdout.write(header_line)
+               header_not_written = False
+           process_cond_line(line, machine)
+
+def update_morph_file_name_in_cond(cond_file, neuron_type, morph_file_name):
+    logger.debug("\n {}".format(cond_file))
+    with fileinput.input(files=(cond_file), inplace=True) as f_obj:
+         for line in f_obj:
+           if find_morph_file(line):
+              line = update_morph_file_name(line, neuron_type, morph_file_name)
+           sys.stdout.write(line)
+
+def clone_param_cond_file(src_path, src_file, dest_file):
+    ''' Inputs param_cond_file == string => Absolute path of cond_file.
+    model_path == Path objects => model Path objects.
+    model == string => model name.
+    neuron_type == string => Neuron model.
+    '''
+    src_abs_path = get_file_abs_path(src_path, src_file)
+    if Path(dest_file).is_file(): # Creates a version file of destination file.
+        dest_file = get_file_name_with_version(dest_file)
+        shutil.copy(src_abs_path, dest_file)
+        return dest_file
+    shutil.copy(src_abs_path, dest_file)
+    return dest_file
+
+def clone_and_change_morph_file(param_cond_file, model_path, model, neuron_type, non_conds):
+    ''' Inputs param_cond_file == string => Absolute path of cond_file.
+               model_path == Path objects => model Path objects.
+               model == string => model name.
+               neuron_type == string => Neuron model.
+    '''
+    from ajustador.basic_simulation import morph_morph_file
+    morph_features = ('RM', 'Eleak', 'RA', 'CM')
+    model_obj = make_model_path_obj(model_path, model)
+    morph_file = extract_morph_file_from_cond(param_cond_file, neuron_type)
+    src_morph_file_path = get_file_abs_path(model_path, morph_file)
+    if 'conductance_save' in src_morph_file_path:
+        new_morph_file_path = get_file_name_with_version(src_morph_file_path)
+        morph_morph_file(model_obj, neuron_type, src_morph_file_path, new_file = open(new_morph_file_path,'w'),
+        **{k:v for k,v in non_conds.items() if k in morph_features})
+        return new_morph_file_path
+    morph_morph_file(model_obj, neuron_type, src_morph_file_path, new_file = open(str(model_path/'conductance_save'/morph_file),'w'),
+    **{k:v for k,v in non_conds.items() if k in morph_features})
+    return str(model_path/'conductance_save'/morph_file)
