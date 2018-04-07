8876ed758d1f65e973b3b81a3320e3954107ebcc
sriram161 <skappag@masonlive.gmu.edu>
2018-04-06 22:20:52 -0400

    Initial code for fit_param.

--- ajustador/helpers/save_param/create_fit_param.py
+++ ajustador/helpers/save_param/create_fit_param.py
@@ -8,3 +8,8 @@ from pathlib import Path
 from datetime import datetime
 from ajustador.helpers.loggingsystem import getlogger
 
+create_fit_param(fit_obj, model, neuron_type, store_param_path=None,
+                     fitnum=None, cond_file=None):
+    pass
+
+#How are the parameter values in fit_object?
