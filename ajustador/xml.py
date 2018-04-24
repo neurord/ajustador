7884382018863fb944ae81838a5fc32f67bedd76
Avrama Blackwell <avrama@gmu.edu>
2018-04-24 18:13:09 -0400

    First neurord optimization version that works with multiple model files Still need to fix fitness function to work with multiple files Possibly add model parameter (similar to injection current) to sort on

--- ajustador/xml.py
+++ ajustador/xml.py
@@ -9,10 +9,16 @@ import shlex
 import subprocess
 import os
 from lxml import etree
+import glob    
+import numpy as np
 
 from ajustador import nrd_output  
 from . import optimize, loader
 
+import logging 
+from ajustador.helpers.loggingsystem import getlogger 
+logger = getlogger(__name__) 
+logger.setLevel(logging.INFO)
 
 class XMLParamMechanism(optimize.ParamMechanism):
     def __init__(self, xpath):
@@ -34,7 +40,8 @@ def do_replacements(model, paramset):
         mech = param.mech
         if isinstance(mech, XMLParamMechanism):
             elems = model.xpath(mech.xpath)
-            print(elems)
+            #print(param,elems,elems[0].text)
+            print(param)
             if len(elems) != 1:
                 raise ValueError('xpath matched {} elements'.format(len(elems)))
             elems[0].text = str(param.value)
@@ -53,17 +60,36 @@ def write_model(model, fname):
 
 class NeurordResult(optimize.SimulationResult):
     def __init__(self, filename, features=[]):
-        # model.h5 is used by default, but a .h5 file with different name can be specified
+        # model.h5 is used if only a directory is specified, but a .h5 file with different name can be specified
+
+        print('NeurordResults', filename)
         if os.path.isdir(filename):
             dirname = filename
-            filename = dirname + '/model.h5'
+            filenames = [dirname + '/model.h5']
         else:
+            #case with only a single filename specified
             dirname = os.path.dirname(filename)
+            if filename.endswith('.h5'):
+                filenames=[filename]
+            else:
+                #case with a set of filenames specified
+                exp_set=os.path.basename(filename)
+                filenames=glob.glob(filename+'*.h5')
+                print('NeurordResult, exp_set',exp_set, ', files', filenames)
 
         super().__init__(dirname, features)
 
-        self.output = nrd_output.Output(filename)
+        output=[nrd_output.Output(fname) for fname in filenames]
+        #to do:
+        #output.sort(key=operator.attrgetter('model_num'))
+        self.output = np.array(output)
+        #self.output=nrd_output.Output(filename)
 
+def modelname_to_param(modelname,root_name):
+    root_name_length=len(root_name)
+    model_num=modelname[root_name_length:-4]
+    return model_num
+   
 class NeurordSimulation(optimize.Simulation):
     def __init__(self, dir,
                  *,
@@ -77,14 +103,26 @@ class NeurordSimulation(optimize.Simulation):
                          params=params,
                          features=[])#features)  without '=[]' doesn't work.
 
-        model2 = update_model(model, params)
-        modelfile = self.tmpdir.name + '/model.xml'
-        write_model(model2, modelfile)
-
-        fout = (modelfile[:-4] + '.h5' if modelfile.endswith('.xml')
-                else modelfile + '.h5')
-
-        args = ((modelfile, fout),)
+        ####### Loop over each simulation in the set #######
+        model_names=(glob.glob(model+"*.xml") if not model.endswith('.xml')
+                     else [model])
+        model_set=[]
+        fout_set=[]
+        param_set=[]
+        for model_nm in model_names:
+            model1= open_model(model_nm)
+            model2 = update_model(model1, params) #xml with new parameters
+            model_num=modelname_to_param(model_nm,model)
+            param_set.append(model_num)
+            logger.info('model _name, _num: {} {}'.format(model_nm, model_num))
+            modelfile = self.tmpdir.name + '/model'+str(model_num)+'.xml'  #name for xml with new parameters
+            write_model(model2, modelfile)  #actually write the xml to the modelfile
+            model_set.append(modelfile) #collect all model files into one array
+            fout = (modelfile[:-4] + '.h5' if modelfile.endswith('.xml')
+                    else modelfile + '.h5')
+            fout_set.append(fout)
+        #collect all the args into one tuple, similar to execute_for in optimize
+        args=((mfile,fout,num) for mfile,fout,num in zip(model_set,fout_set,param_set))
 
         if async:
             func = optimize.exe_map(single=False, async=True)
@@ -97,16 +135,16 @@ class NeurordSimulation(optimize.Simulation):
     def _set_result(self, result):
         tag = os.path.join(self.tmpdir.name, '.complete')
         open(tag, 'w').close()
-
-        assert len(result) == 1
-        self.output = nrd_output.Output(result[0])
+        
+        output=[nrd_output.Output(result[i]) for i in range(len(result))]
+        self.output=np.array(output,dtype=object)
 
     @classmethod
     def make(cls, *, dir, model, measurement, params):
         return cls(dir=dir, model=model, params=params)
 
 def execute(p):
-    modelfile, outfile = p
+    modelfile, outfile, num = p
 
     cmdline = ['neurord', modelfile, outfile]
     print('+', ' '.join(shlex.quote(term) for term in cmdline), flush=True)
