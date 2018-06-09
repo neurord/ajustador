269f765a1236e91ce18f6b44fe4e3168aa4eb1d1
sriram161 <skappag@masonlive.gmu.edu>
2018-06-09 13:04:54 -0400

    modifed comment to sync with code flow.

--- ajustador/basic_simulation.py
+++ ajustador/basic_simulation.py
@@ -60,9 +60,7 @@ def cond_setting(s):
     return chan, comp, rhs
 
 def chan_setting(s):
-    ''' Splits channel voltagent dependent parameters to four parts.
-        'NaF, vshift, X=123.4' → ('NaF', 'vshift', 'X', 123.4)
-    '''
+        "'NaF, vshift, X=123.4' → ('NaF', 'vshift', 'X', 123.4)"
     logger.debug("logger in chan_settings!!!")
     lhs, rhs = s.split('=', 1)
     rhs = float(rhs)
