ded948a19fb69d1aaa23555c108059705adaec21
sriram161 <skappag@masonlive.gmu.edu>
2018-06-20 16:44:10 -0400

    Sync-up with master branch.

--- ajustador/basic_simulation.py
+++ ajustador/basic_simulation.py
@@ -221,7 +221,7 @@ def setup(param_sim, model):
                          model.param_cond.NAME_SOMA)
 
     if param_sim.hsolve and model.calYN:
-        calcium.fix_calcium(model.neurontypes(), model)
+        calcium.fix_calcium(util.neurontypes(model.param_cond), model)
 
     return pg, writer
 
@@ -257,7 +257,7 @@ def main(args):
     global param_sim, pulse_gen
     param_sim = option_parser().parse_args(args)
     model = importlib.import_module('moose_nerp.' + param_sim.model)
-    model.neurontypes([param_sim.neuron_type])
+    model.param_cond.neurontypes=util.neurontypes(model.param_cond,[param_sim.neuron_type])
     logger.debug("param_sim::::::::: {}".format(param_sim))
     pulse_gen, hdf5writer = setup(param_sim, model)
     run_simulation(param_sim.injection_current[0], param_sim.simtime, param_sim, model)
