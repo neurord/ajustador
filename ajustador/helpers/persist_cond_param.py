4ad1815e7cd42d51284cddf99e78f5cc2b05e0d0
sriram161 <skappag@masonlive.gmu.edu>
2018-06-27 14:15:57 -0400

    Added in comments and fixed temp file problem.

--- ajustador/helpers/persist_cond_param.py
+++ ajustador/helpers/persist_cond_param.py
@@ -1,9 +1,11 @@
+# TODO add descriptor and explain.
 
 from ajustador.helpers.save_param.create_fit_param import create_fit_param
 from ajustador.helpers.save_param.create_npz_param import create_npz_param
 
 def persist_cond_param(object_type, model, neuron_type, **kwargs):
     if object_type == "fit":
+        # TODO Add a fit object processing.
         pass
     elif object_type == "npz":
         npz_file = kwargs.get('npz_file') if kwargs.get('npz_file') else raise ValueError('npz_file needed!!!')
--- test_scripts/tau_all_squid_opt_1P.py
+++ test_scripts/tau_all_squid_opt_1P.py
@@ -27,7 +27,8 @@ os.chdir(neuron_file_loc)
 print("squid data keys:", squid_exp.data.keys())
 exp_to_fit = squid_exp.data[dataname] # TODO check for data!!! get professors help.
 
-tmpdir='/tmp/fit'+modeltype+'-'+ntype+'-'+dataname+'F'
+# tempdir must allways be a 2 level directory /tmp/<your existing dir> thats it.
+tmpdir='/tmp/Sriramsagar'+modeltype+'-'+ntype+'-'+dataname+'F'
 
 ######## setup parameters for fitness ############
 
