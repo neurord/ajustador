9330a9a06088e98242d1c94231eaaed2a5d2ddf5
sriram161 <skappag@masonlive.gmu.edu>
2018-07-13 12:40:14 -0400

    scaleing_factors.py: Fixed indentation error.

--- ajustador/helpers/scaleing_factors.py
+++ ajustador/helpers/scaleing_factors.py
@@ -9,20 +9,20 @@ from ajustador.helpers.loggingsystem import getlogger
 logger = getlogger(__name__)
 
 def get_units_scale_factor(eng_units):
-        ''' Fuction to get suitable multiplier using the scale characater code from units
+    ''' Fuction to get suitable multiplier using the scale characater code from units
             information.
 
             @Usage:
                    get_units_scale_factor(eng_units='mA') -> 10e-3
-        '''
-	units_prefix = {"y":1e-24, "z":1e-21, "a":1e-18, "f":1e-15, "p": 1e-12,
-	                "n":1e-9, "u":1e-6, "µ":1e-6, "m":1e-3, "c":1e-2, "d":0.1,
-                        "h":100, "k":1000, "M":1e6, "G":1e9, "T":1e12, "P":1e15,
-                        "E":1e18, "Z":1e21, "Y":1e14}
-	try:
-            prefix = eng_units.strip('(')[0]
-            return units_prefix[prefix]
-	except (KeyError, IndexError):
-            logger.warn("Provided units {}! please check your input units in csv.".format(eng_units))
-            logger.warn("Assuming SI units! Using scaling factor 1.0!")
-            return 1.0
+    '''
+    units_prefix = {"y":1e-24, "z":1e-21, "a":1e-18, "f":1e-15, "p": 1e-12,
+                    "n":1e-9, "u":1e-6, "µ":1e-6, "m":1e-3, "c":1e-2, "d":0.1,
+                    "h":100, "k":1000, "M":1e6, "G":1e9, "T":1e12, "P":1e15,
+                    "E":1e18, "Z":1e21, "Y":1e14}
+    try:
+        prefix = eng_units.strip('(')[0]
+        return units_prefix[prefix]
+    except (KeyError, IndexError):
+        logger.warn("Provided units {}! please check your input units in csv.".format(eng_units))
+        logger.warn("Assuming SI units! Using scaling factor 1.0!")
+        return 1.0
